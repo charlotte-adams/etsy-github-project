faefc85e7f0835707314c8ea8def63e330691792
Charlotte Warfel <charlottewarfel@gmail.com>
2019-04-17 15:35:50 -0700

    build Home Component

---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export class Home extends Component {
+  render() {
+    return <div className="home-page">This is the home page</div>;
+  }
+}
