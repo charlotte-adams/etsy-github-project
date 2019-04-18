0e09feb2ab6dc43d9097ab881f15123dd9a3ce72
Charlotte Warfel <charlottewarfel@gmail.com>
2019-04-18 13:07:19 -0700

    create AllProductsContainer and AllProducts Components

--- src/AllProducts.js
+++ src/AllProducts.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 
 export class AllProducts extends Component {
   render() {
-    return (
-      <div className="all-products-page">This is the All Products Page</div>
-    );
+    return <div className="all-products-page">This is All Products</div>;
   }
 }
