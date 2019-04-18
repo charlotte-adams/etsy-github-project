dfd1155569d22063d997e7885a9222810b01a3d5
Charlotte Warfel <charlottewarfel@gmail.com>
2019-04-18 13:09:44 -0700

    refine Home Component by adding FeaturedItem and SingleProduct

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import "./Home.css";
+import { FeaturedItem } from "./FeaturedItem.js";
+import { SingleProduct } from "./SingleProduct.js";
 
 export class Home extends Component {
   render() {
-    return <div className="home-page">This is the home page</div>;
+    return (
+      <div className="home-page-wrapper">
+        <div className="home-page-featured-each-container">
+          <FeaturedItem />
+          <SingleProduct />
+        </div>
+      </div>
+    );
   }
 }
