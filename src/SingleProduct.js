e4ced851f06012778a994e72fe3758da96ddf87a
Charlotte Warfel <charlottewarfel@gmail.com>
2019-04-18 13:15:32 -0700

    create SingleProduct Component

---
+++ src/SingleProduct.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./SingleProduct.css";
+
+export class SingleProduct extends Component {
+  render() {
+    return (
+      <div className="home-page-single-product-card-wrapper">
+        Single Product Card Wrapper
+        <div className="each-product-card-container">Each Product Card</div>
+      </div>
+    );
+  }
+}
